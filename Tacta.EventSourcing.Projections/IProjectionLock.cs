3b672134fc7b814bb880da735d857c743712595a
Bolic Damir <damir.bolic@cegeka.com>
2019-05-07 12:54:45 +0200

    Projection Lock feature added

---
+++ Tacta.EventSourcing.Projections/IProjectionLock.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Tacta.EventSourcing.Projections
+{
+    public interface IProjectionLock
+    {
+        Task<bool> IsActiveProjection(string activeIdentity);
+    }
+}
